c5d1a03d39533500fe6bf63f4fdb940024afbed5
CONSTANT <shavkatov.olimboy@mail.ru>
2021-02-03 19:21:01 +0500

    add docker (compose), requirements; fixed github django secret key warnigs;

--- svetuz_bot/settings.py
+++ svetuz_bot/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'kf=(mp%cwbc8hq8l6ks_lq++mey3a%xrs-@2ap!m-48jh@(kbn'
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
