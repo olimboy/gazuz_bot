774a2a599c6915b2b3f811bb11953c2b3606921e
CONSTANT <shavkatov.olimboy@mail.ru>
2021-02-04 16:18:11 +0500

    docker: db migrate fixed

--- svetuz_bot/settings.py
+++ svetuz_bot/settings.py
@@ -29,8 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
+DEBUG = os.getenv('DEBUG') == 'True'
 ALLOWED_HOSTS = ['*']
 
 
